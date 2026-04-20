73a913b1130c277b5d716d2b38b22f873ef4ca20
wwmjw <771061970@qq.com>
2026-04-20 17:03:50 +0800

    Update data.js

--- data.js
+++ data.js
@@ -1,5 +1,15 @@
 const worksData = [
     {
+        id: 14,
+        title: "新作品展示",
+        category: "poster",
+        image: "https://wwmjw-1423348972.cos.ap-guangzhou.myqcloud.com/images/海报/new-work.jpg",
+        images: [
+            "https://wwmjw-1423348972.cos.ap-guangzhou.myqcloud.com/images/海报/new-work.jpg"
+        ],
+        description: "新添加的作品展示"
+    },
+    {
         id: 1,
         title: "紫花油 510海报",
         category: "poster",
